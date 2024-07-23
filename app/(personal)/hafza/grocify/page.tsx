631b6f93f5588dfba73026931a41238e80a26af6
hafza azeem <hafza.azeemuddin@outlook.com>
2024-07-23 13:59:41 +0500

    Grocify change

--- app/(personal)/hafza/grocify/page.tsx
+++ app/(personal)/hafza/grocify/page.tsx
@@ -2,6 +2,7 @@ export default function GrocifyRoute() {
   return (
     <div>
       <h1>Grocify page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio ad sint nostrum aspernatur vero quos doloribus ratione nisi repudiandae voluptates necessitatibus numquam sunt accusamus est at, tenetur totam voluptatem! Magni?</p>
     </div>
   );
 }
